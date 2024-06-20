943a72b8c3976246702a8d8fb85653861e7d8670
Rishabh Ratan <rishabhkarondiya@gmail.com>
2024-06-20 18:59:17 +0530

    body-parser added

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const dotenv = require('dotenv')
 const cors = require('cors')
+const bodyParser = require('body-parser')
 dotenv.config()
 const app = express()
 
@@ -19,6 +20,7 @@ const corsOption={
 }
 app.use(cors())
 app.use(express.json())
+app.use(bodyParser.json())
 
 app.use('/',authRoute)
 app.use("/api/progress", progressRoute)
