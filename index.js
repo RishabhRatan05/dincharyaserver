f46d46ef8b6a0d2b400a7592cd252cbd8f496914
Rishabh Ratan <rishabhkarondiya@gmail.com>
2024-08-09 21:56:39 +0530

    checking removed

--- index.js
+++ index.js
@@ -1,32 +1,32 @@
-const express = require('express')
-const dotenv = require('dotenv')
-const cors = require('cors')
-const bodyParser = require('body-parser')
+const express = require("express")
+const dotenv = require("dotenv")
+const cors = require("cors")
+const bodyParser = require("body-parser")
 dotenv.config()
 const app = express()
 
 const PORT = process.env.PORT || 8000
-const url = process.env.URL 
+const url = process.env.URL
 const profileRoute = require("./routes/profile.js")
 const progressRoute = require("./routes/progress.js")
-const authRoute = require('./routes/auth.js')
+const authRoute = require("./routes/auth.js")
 
-const connectDB = require('./utils/conn.js')
-const {checkLoggedInUser} = require('./middlewares/auth.js')
+const connectDB = require("./utils/conn.js")
+const { checkLoggedInUser } = require("./middlewares/auth.js")
 connectDB()
 
-const corsOption={
-    origin:url,
-    method:"GET PUT POST DELETE"
+const corsOption = {
+  origin: url,
+  method: "GET PUT POST DELETE",
 }
 app.use(cors())
 app.use(express.json())
 app.use(bodyParser.json())
 
-app.use('/',authRoute)
-app.use("/api/progress", checkLoggedInUser, progressRoute)
-app.use("/api/profile",checkLoggedInUser,profileRoute)
+app.use("/", authRoute)
+app.use("/api/progress", progressRoute)
+app.use("/api/profile", profileRoute)
 
-app.listen(PORT,()=>{
-    console.log( `server started at port : ${PORT}`   )
+app.listen(PORT, () => {
+  console.log(`server started at port : ${PORT}`)
 })
